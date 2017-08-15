e6706c3691037a1b3e735a7c0325f6a9a2455470
VimalRaj Selvam <email2vimalraj@gmail.com>
2017-08-15 18:35:54 +0530

    Added router with Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Header from './Header'
 import ProjectList from './ProjectList'
 
 class App extends Component {
   render() {
     return (
-      <ProjectList />
+      <div className='center w85'>
+        <Header />
+        <div className='ph3 pv1 background-gray'>
+          <Switch>
+            <Route exact path='/' component={ProjectList} />
+          </Switch>
+        </div>
+      </div>
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router'
+
+class Header extends Component {
+
+    render() {
+        return (
+            <div className='flex pa1 justify-between nowrap orange'>
+                <div className='flex flex-fixed black'>
+                    <div className='fw7 mr1'>ExtentX</div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default withRouter(Header)
